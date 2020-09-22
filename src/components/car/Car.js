212e58467a77a634c986cebbcc79b77b6eef78ae
Rauf Ismayilov <rauf.raymond110@gmail.com>
2020-09-22 19:16:24 +0400

    test

---
+++ src/components/car/Car.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+
+export default function Car(){
+    return(
+        <div className="container">
+            <h1 className="header">Car component</h1>
+        </div>
+    )
+}
---
+++ src/components/user/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style.module.css";
+
+
+function User(){
+    
+    return(
+        <div className={styles.container}>
+            <h1 className={styles.header}>Car component</h1>
+        </div>
+    )
+}
+
+export default User;
