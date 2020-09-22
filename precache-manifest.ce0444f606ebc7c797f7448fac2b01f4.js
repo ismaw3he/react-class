74c6f18b04f178f0f97db4c3d120ad7c43b0cd17
Rauf Ismayilov <rauf.raymond110@gmail.com>
2020-09-22 19:21:42 +0400

    Updates

---
+++ precache-manifest.ce0444f606ebc7c797f7448fac2b01f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8df2da4604c88505975686f2ef5f8fb",
+    "url": "/react-class/index.html"
+  },
+  {
+    "revision": "bc9ef03e86a7d86dd439",
+    "url": "/react-class/static/css/main.0610f75c.chunk.css"
+  },
+  {
+    "revision": "458a36ce3be4493d23dc",
+    "url": "/react-class/static/js/2.84ae0457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-class/static/js/2.84ae0457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9ef03e86a7d86dd439",
+    "url": "/react-class/static/js/main.d83b5b7a.chunk.js"
+  },
+  {
+    "revision": "6a85806f2e1ef029b157",
+    "url": "/react-class/static/js/runtime-main.3da9ab92.js"
+  }
+]);
